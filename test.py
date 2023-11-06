82770d5dfed0a49157e931d898d69d3f0ae838b7
Kelvin <kelvin@Kelvins-MacBook-Air.local>
2023-11-06 02:23:22 -0500

    updates to reporting table

--- test.py
+++ test.py
@@ -54,8 +54,7 @@ for team in team_info:
 team_record_df
 
 
-coach_data = commonteamroster.CommonTeamRoster(team_id=
-1610612737, season=2022)
+coach_data = commonteamroster.CommonTeamRoster(team_id=1610612737, season=2022)
 coach_data_df = coach_data.coaches.get_data_frame()
 coach_name = coach_data_df[coach_data_df['COACH_TYPE'] == 'Head Coach']['COACH_NAME'][0]
 coach_data_df
@@ -68,12 +67,93 @@ from nba_api.stats.endpoints import playoffpicture
 playoff_picture = playoffpicture.PlayoffPicture(season_id='22005')
 
 # Call the get_data_frames method to retrieve the data frames
-data_frames = playoff_picture.east_conf_standings.get_data_frame()
-teams = data_frames['TEAM']
-wins = data_frames['WINS']
-wins
+data = pd.DataFrame(columns=['East Wins', 'East Team'])
+playoff_picture = playoffpicture.PlayoffPicture(season_id=22021)
+playoff_picture_east_df = playoff_picture.east_conf_standings.get_data_frame()
+playoff_picture_east_df
+east_team_wins = playoff_picture_east_df['WINS']
+east_team = playoff_picture_east_df['TEAM']
+
+data_df = pd.DataFrame({
+    'East Wins': east_team_wins,
+    'East Team': east_team
+})
+
+data_df
+data = pd.concat([data, data_df])
+data
+
+
 # Now, data_frames contains the data frames with the playoff picture data[]
 
 data = []
 data.append([teams, wins])
 pd.DataFrame(data)
+
+
+for season in range(2005, 2024):
+    print('2'+f"{(season)}")
+
+
+import pandas as pd
+from nba_api.stats.endpoints import playoffpicture
+
+# Initialize an empty DataFrame to store the data
+playoff_picture_df = pd.DataFrame(columns=['Season', 'Team', 'Team ID', 'Wins', 'Losses', 'Win PCT', 'Clinched'])
+
+for season in range(2005, 2024):
+    playoff_picture = playoffpicture.PlayoffPicture(season_id='2' + str(season))
+    
+    playoff_picture_east_df = playoff_picture.east_conf_standings.get_data_frame()
+    east_team_wins = playoff_picture_east_df['WINS']
+    east_team_losses = playoff_picture_east_df['LOSSES']
+    east_team_pct = playoff_picture_east_df['PCT']
+    east_team = playoff_picture_east_df['TEAM']
+    east_team_id = playoff_picture_east_df['TEAM_ID']
+    east_team_clinched_playoffs = playoff_picture_east_df['CLINCHED_PLAYOFFS']
+    
+    east_df = pd.DataFrame({
+        'Season': f"{season}-{str(season + 1)[-2:]}",
+        'Team': east_team,
+        'Team ID': east_team_id,
+        'Wins': east_team_wins,
+        'Losses': east_team_losses,
+        'Win PCT': east_team_pct,
+        'Clinched': east_team_clinched_playoffs
+    })
+    
+    playoff_picture_west_df = playoff_picture.west_conf_standings.get_data_frame()
+    west_team_wins = playoff_picture_west_df['WINS']
+    west_team_losses = playoff_picture_west_df['LOSSES']
+    west_team_pct = playoff_picture_west_df['PCT']
+    west_team = playoff_picture_west_df['TEAM']
+    west_team_id = playoff_picture_west_df['TEAM_ID']
+    west_team_clinched_playoffs = playoff_picture_west_df['CLINCHED_PLAYOFFS']
+    
+    west_df = pd.DataFrame({
+        'Season': f"{season}-{str(season + 1)[-2:]}",
+        'Team': west_team,
+        'Team ID': west_team_id,
+        'Wins': west_team_wins,
+        'Losses': west_team_losses,
+        'Win PCT': west_team_pct,
+        'Clinched Playoffs': west_team_clinched_playoffs,
+        'Clinched Conference': west_team_clinched_conference,
+        'Clinched '
+    })
+    
+    # Concatenate East and West DataFrames vertically
+    playoff_picture_df = pd.concat([playoff_picture_df, east_df, west_df])
+
+# Reset index if needed
+playoff_picture_df.reset_index(drop=True, inplace=True)
+
+# Display the final DataFrame
+print(playoff_picture_df)
+
+playoff_picture_east_df = playoff_picture.get_data_frames()
+data = pd.DataFrame(playoff_picture_east_df)
+
+from nba_api.stats.endpoints import playoffpicture
+WestConfPlayoffPicture_df = playoff_picture.west_conf_playoff_picture.get_data_frame()
+WestConfPlayoffPicture_df['HIGH_SEED_RANK']
